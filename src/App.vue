ea93aa3af8c6753216945ea5bd055e13f2bfc019
Yuto Suzuki <mokkumokku99@gmail.com>
2020-12-02 22:25:41 +0900

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,18 @@
 <template>
   <div id="app">
-    {{user.username}}-{{fullName}}
-    <strong>Followers:</strong>{{followers}}
-    <button @click='followUser'>
-      Follow
-    </button>
+    <UserProfile/>
   </div>
 
 </template>
 
 <script>
+import UserProfile from "./components/UserProfile";
 
 export default {
-  name: 'App',
-  data(){
-  return{
-    followers: 0,
-    user:{
-      id:1,
-      username:"_yuto",
-      firstname:"Yuto",
-      lastname:"Suzuki",
-      email:"mokkumokku99@gmail.com",
-      isAdmin: true
-      }
-    }
-  },
-  computed:{
-    fullName(){
-        return `${this.user.firstname} ${this.user.lastname}`;
-
-    }
-
-  },
-  methods:{
-    followUser(){
-       this.followers++;
-    }
-  },
-
-  mounted(){
-    this.followUser();
-
-  }
+  name: "App",
+  components:{UserProfile}
 }
+
 </script>
 
 <style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{user.username}}</h1>
+      <div class="user-profile__follwer-count">
+        <strong>Followers:</strong>{{followers}}
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+  return{
+    followers: 0,
+    user:{
+      id:1,
+      username:"_yuto",
+      firstname:"Yuto",
+      lastname:"Suzuki",
+      email:"mokkumokku99@gmail.com",
+      isAdmin: true
+      }
+    }
+  },
+  watch:{
+    followers(newFollowerCount, oldFollowerCount){
+      if (oldFollowerCount < newFollowerCount){
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed:{
+    fullName(){
+        return `${this.user.firstname} ${this.user.lastname}`;
+
+    }
+
+  },
+  methods:{
+    followUser(){
+       this.followers++;
+    }
+  },
+
+  mounted(){
+    this.followUser();
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  color: #2c3e50;
+  flex-direction: column;
+  text-align:center;
+}
+</style>
